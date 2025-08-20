51d086e78abd12b40513adc28739ab2a5b1b3a0b
ym0c4 <melanieyu0128@gmail.com>
2025-08-20 13:17:47 +0200

    Finished python version

--- WordleApp.py
+++ WordleApp.py
@@ -1,11 +1,32 @@
 import random
 
 name = input("What is your name?:")
-print("You have 5 chances in total to guess the right word")
+print("You have 10 chances in total to guess the right word")
 print(f"Good luck with playing {name}!!")
 
 words = ['Action', 'Film', 'Drone', 'Web', 'Security', 'Mirrorless', 'Lens', 'Sensor', 'Shutter', 'Viewfinder', 'ISO', 'Aperture', 'Image','Autofocus', 'Zoom', 'Flash', 'Image', 'Microphone', 'Expose', 'White Balance', 'Megapixels', 'Format', 'Histogram','Canon','Nikon','Sony','Fujifilm','Panasonic','GoPro','Lumix','Samsung']
 
-word = random.choice(words)
-guess = ""
-turns = 5
+guessword = random.choice(words)
+displayed_word = ["*"] * len(guessword)
+turns = 10
+
+while turns > 0:
+    for char in displayed_word:
+        print(char, end = "")
+    print()
+
+    guess = input("Guess a charcter:")
+    
+    if guess.lower() in guessword.lower():
+        for i in range(len(guessword)):
+            if guessword[i].lower() == guess.lower():
+                displayed_word[i] = guessword[i]
+    else:
+        turns -= 1
+        print(f"Wrong! You now have {turns} remaining guesses")
+
+    if "*" not in displayed_word:
+        print(f"You won! The correct word was: {guessword}")
+        break
+else:
+    print(f"You lost the game! The word was: {guessword}")
