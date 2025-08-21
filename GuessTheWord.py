98ae83f642ad57fcde3be3d78d18c7de4c75f958
ym0c4 <melanieyu0128@gmail.com>
2025-08-21 11:43:42 +0200

    Initialized ReadMe

--- GuessTheWord.py
+++ GuessTheWord.py
@@ -1,21 +1,21 @@
 import random
 
 name = input("What is your name?:")
-print("You have 10 chances in total to guess the right word")
+print("You have 11 chances in total to guess the right word! Hint: Camera")
 print(f"Good luck with playing {name}!!")
 
 words = ['Action', 'Film', 'Drone', 'Web', 'Security', 'Mirrorless', 'Lens', 'Sensor', 'Shutter', 'Viewfinder', 'ISO', 'Aperture', 'Image','Autofocus', 'Zoom', 'Flash', 'Image', 'Microphone', 'Expose', 'White Balance', 'Megapixels', 'Format', 'Histogram','Canon','Nikon','Sony','Fujifilm','Panasonic','GoPro','Lumix','Samsung']
 
 guessword = random.choice(words)
 displayed_word = ["*"] * len(guessword)
-turns = 10
+turns = 11
 
 while turns > 0:
     for char in displayed_word:
         print(char, end = "")
     print()
 
-    guess = input("Guess a charcter:")
+    guess = input("Guess a letter:")
     
     if guess.lower() in guessword.lower():
         for i in range(len(guessword)):
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Wordle
-Guess the word!
+# Guess the word (Project)
+Welcome to my initial project on GitHub. 
+This is a small python project where you can play a "guess the word" game based on brands, items, terms etc. related to cameras. This game is text-based and can easily be played directly in the terminal.
+
+## Introduction
+### Rules
+- You have 11 chances to guess the correct word.  
+- Each correct letter guessed, it will reveal its place(s) in the word.  
+- If you run out of turns before completing the word, you lose.  
+- All words are related to *cameras*.
+
+### Run on terminal
+1. Make sure you have Python 3 installed
+2. Clone this repository in the terminal with either https://github.com/ym0c4/Wordle.git
+or git@github.com:ym0c4/Wordle.git
+3. Open the termianal and navigate to where you installed it
+4. Run the program with: python3 GuessTheWord.py
+
+Good luck!
